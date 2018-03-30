8720323d18d0431b2086f58fd290b56a1612dd37
Daisy-bbfish <daisy.bbfish@gmail.com>
2018-03-30 11:03:34 +0800

    project

---
+++ DS09-W4/server.R
@@ -0,0 +1,28 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+Average_Factor<-function(Databases,Data_Analytics,Project_Management) {(Databases + Data_Analytics + Project_Management)/3}
+
+diagnostic_f<-function(Databases, Data_Analytics, Project_Management){
+        Average_value<-(Databases + Data_Analytics + Project_Management)/3
+        ifelse(Average_value<70,"Failed", ifelse(Average_value >=70 & Average_value<80 ,"Database Adminstrator Position",ifelse(Average_value >= 80 & Average_value < 90 ,"Data Adminstrator Position",ifelse(Average_value>=90 ,"Business Intelligence Manager Position")))
+        )}
+
+shinyServer(
+        function(input, output) {
+                
+                output$inputdatabasesmark <- renderPrint({input$Databases})
+                output$inputdataAnalyticsmark <- renderPrint({input$Data_Analytics})
+                output$inputprojectmanagementmark <- renderPrint({input$Project_Management})
+                output$estimation <- renderPrint({Average_Factor(input$Databases,input$Data_Analytics,input$Project_Management)})
+                output$diagnostic <- renderPrint({diagnostic_f(input$Databases,input$Data_Analytics,input$Project_Management)})
+        } 
+)
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# DS09-W4
-Shiny Application and Reproducible Pitch
+# DS09-W4 Shiny Application and Reproducible Pitch
+
+## Instructions
+
+This peer assessed assignment has two parts. First, you will create a Shiny application and deploy it on Rstudio's servers. Second, you will use Slidify or Rstudio Presenter to prepare a reproducible pitch presentation about your application.
+
+## My Shiny Application
+
+Write a shiny application with associated supporting documentation. The documentation should be thought of as whatever a user will need to get started using your application. Deploy the application on Rstudio's shiny server Share the application link by pasting it into the provided text box Share your server.R and ui.R code on github The application must include the following:
+
+https://daisy-bbfish.shinyapps.io/DS09-W4/
+
+## My Reproducible Pitch Presentation
+
+OK, you've made your shiny app, now it's time to make your pitch. You get 5 slides (inclusive of the title slide) to pitch a your app. You're going to create a web page using Slidify or Rstudio Presenter with an html5 slide deck.
+
+http://rpubs.com/DaisyWu/DS09-W4
